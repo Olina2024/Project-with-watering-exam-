8222acb3084518177f6fdca4c72e6615e4f4c2b4
Jing Ouyang <ouyangjing1983@gmail.com>
2025-12-14 20:42:43 +0100

    Add water level and additional pin definitions

--- gpio.h
+++ gpio.h
@@ -2,8 +2,13 @@
 #define SCL 5
 #define LM75_ADDR 0x48
 #define Anlog_Pin 9
+#define Anlog_Waterlevel_Pin 10
+#define DC_Pin 13
+#define LED_Pin 16
+#define DRY_SOIL 3800
+#define WET_SOIL 600
+#define WATER_EMPTY 3900   // sensor in air
+#define WATER_FULL  600    // sensor fully in water
 float temperature;
 float humidity;
-
-float temperature_threshold = 20.0;
-float humidity_threshold = 70.0;
+float waterlevel;
