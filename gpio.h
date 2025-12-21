628da3d868d432a8485e2a81623666af8364c2cb
Jing Ouyang <ouyangjing1983@gmail.com>
2025-12-21 19:53:53 +0100

    Update gpio.h with new constants and structures
    Added new constants for Fan_Pin and updated soil moisture thresholds. Introduced timing and FreeRTOS structures for sensor data handling.

--- gpio.h
+++ gpio.h
@@ -3,12 +3,31 @@
 #define LM75_ADDR 0x48
 #define Anlog_Pin 9
 #define Anlog_Waterlevel_Pin 10
+#define Fan_Pin 17
 #define DC_Pin 13
 #define LED_Pin 16
-#define DRY_SOIL 3800
-#define WET_SOIL 600
-#define WATER_EMPTY 3900   // sensor in air
-#define WATER_FULL  600    // sensor fully in water
-float temperature;
-float humidity;
-float waterlevel;
+#define DRY_SOIL 20
+#define WET_SOIL 2155
+#define WATER_EMPTY 5   // sensor in air
+#define WATER_FULL  1371    // sensor fully in water
+
+// ---------- Timing ----------
+unsigned long lastSensorRead = 0;
+const unsigned long SENSOR_INTERVAL = 2000;
+
+// ---------- Sensor data ----------
+float temperature = 0;
+float humidity = 0;
+float waterlevel = 0;
+
+// ---------- FreeRTOS ----------
+TaskHandle_t influxTaskHandle;
+QueueHandle_t influxQueue;
+
+// ---------- Struct for queue ----------
+typedef struct {
+  float temperature;
+  float humidity;
+  float waterlevel;
+} SensorData;
+
